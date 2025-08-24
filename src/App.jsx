8f1bced3bcc2122ef6235ac7a8b0cc0614ed14bf
gehad <gehadadel6111@gmail.com>
2025-08-24 21:26:12 +0300

    finish the project

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,19 @@ import Typography from '@mui/material/Typography';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+// date and time
+import "moment/min/locales";
+import moment from 'moment/moment';
+
+// translation hook
+import { useTranslation } from 'react-i18next';
+
+
+
+moment.locale("ar")
 function App() {
+  const { t, i18n } = useTranslation();
+  const [dateAndTime, setDateAndTime] = useState("")
   const theme = createTheme({
     typography: {
       fontFamily: ["IBM"]
@@ -23,6 +35,7 @@ function App() {
       }
     }
   });
+  const [locale, setLocale] = useState("ar")
   const [temp, setTemp] = useState({
     number: null,
     min: null,
@@ -31,7 +44,24 @@ function App() {
     icon: null
   })
   let cancelAxios = null
+  // event handellers
+  function handleLanguageClick() {
+    if (locale == "en") {
+      setLocale("ar")
+      i18n.changeLanguage("ar")
+      moment.locale("ar")
+    } else {
+      setLocale("en")
+      i18n.changeLanguage("en")
+      moment.locale("en")
+    }
+    setDateAndTime(moment().format('dddd MMMM YYYY'))
+  }
   useEffect(() => {
+    i18n.changeLanguage("ar")
+  }, [])
+  useEffect(() => {
+    setDateAndTime(moment().format('dddd MMMM YYYY'))
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=30.06263&lon=31.24967&appid=7d86729136d5d45dea476fbc6ddcf546`, {
       cancelToken: new axios.CancelToken((c) => {
         cancelAxios = c
@@ -58,20 +88,20 @@ function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Container maxWidth="sm" dir="rtl">
+        <Container maxWidth="sm" dir={locale == "ar" ? "rtl" : "ltr"}>
           <div className="contentContainer" style={{ width: "100%" }} >
             {/* card */}
             <div className="card" style={{ background: "rgb(28 52 91 / 36%)", borderRadius: "5px", boxShadow: "10px 10px 10px rgba(0,0,0,0.5)" }}>
               {/* name & time */}
-              <div style={{ display: "flex", justifyContent: "start", alignItems: "end", }}>
+              <div style={{ display: "flex", justifyContent: "start", alignItems: "center", }}>
                 <div style={{ margin: "0px 20px" }}>
-                  <Typography variant="h2" component="h2" style={{ fontWeight: 400, textDecoration: "capetalize" }}>
-                    القاهرة
+                  <Typography variant="h2" component="h2" style={{ fontWeight: 400, textTransform: "capetalize" }}>
+                    {t("Cairo")}
                   </Typography>
                 </div>
                 <div style={{ margin: "0px 20px" }}>
                   <Typography variant="h5" component="h5" style={{ fontWeight: 200 }}>
-                    الأحد-23-2025
+                    {dateAndTime}
                   </Typography>
                 </div>
               </div>
@@ -84,22 +114,22 @@ function App() {
                   {/* temp */}
                   <div style={{ display: "flex" }}>
                     <Typography variant="h2" component="h2" style={{ fontWeight: 200, margin: "15px" }}>
-                      {temp.number}
+                      {temp.number}°
                     </Typography>
                     <img src={temp.icon} alt={temp.description} />
                   </div>
                   {/* state */}
                   <div>
                     <Typography variant="h6" component="h6" style={{ fontWeight: 200, margin: "15px" }}>
-                      {temp.description}
+                      {t(temp.description)}
                     </Typography>
                   </div>
                   {/* min & max */}
                   <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
 
-                    <h6 style={{ margin: "0px 10px", fontWeight: "400" }}>الصغرى {temp.min}</h6>
+                    <h6 style={{ margin: "0px 10px", fontWeight: "400" }}>{t("Min")} {temp.min}</h6>
                     <h6 style={{ margin: "0px 10px", fontWeight: "400" }}> |</h6>
-                    <h6 style={{ margin: "0px 10px", fontWeight: "400" }}>الكبرى {temp.max}</h6>
+                    <h6 style={{ margin: "0px 10px", fontWeight: "400" }}>{t("Max")} {temp.max}</h6>
 
                   </div>
                 </div>
@@ -112,7 +142,8 @@ function App() {
             {/* card */}
             {/* button */}
             <div style={{ width: "100%", display: "flex", justifyContent: "end", alignItems: "center" }}>
-              <Button variant="text" style={{ color: "rgb(28 52 91 / 36%)", marginTop: "20px", padding: "10px" }}>إنجليزي</Button>
+              <Button variant="text" style={{ color: "rgb(28 52 91 / 36%)", marginTop: "20px", padding: "10px", textTransform: "capitalize" }}
+                onClick={handleLanguageClick}>{locale == "en" ? "Arabic" : "إنجليزي"}</Button>
             </div>
           </div>
         </Container>
---
+++ src/i18n.js/index.js
@@ -0,0 +1,17 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import ar from "./locals/ar.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    ar: { translation: ar },
+  },
+  lng: "ar",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
